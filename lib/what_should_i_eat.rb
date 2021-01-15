3f37acae0cda37a96f921937a24d43db77176e00
jessica_mach <Jessica.Mach@outlook.com>
2021-01-15 14:22:46 -0800

    Moved printing code to separate module

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "what_should_i_eat/version"
+require 'what_should_i_eat/printing'
 require 'http'
 require 'json'
 
@@ -8,9 +9,9 @@ module WhatShouldIEat
   class Error < StandardError; end
   # Your code goes here...
   search_result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
-
   recipes = JSON.parse(search_result)
+  name = recipes.first['items'].first['label'] 
+  url = recipes.first['items'].first['url']
 
-  puts recipes.first['items'].first['label']
-  puts recipes.first['items'].first['url']
+  Printing.print name, url
 end
